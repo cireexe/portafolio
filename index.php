3dc09772dea3b2de999f0c23fd6f34c20688c43c
cireexe <celina.cc1501@gmail.com>
2023-12-18 12:36:46 -0300

    titulo actualizacion

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Tu Nombre - Desarrollador Web</title>
+    <title>Celina - Desarrollador Web</title>
 </head>
 
 <body>
